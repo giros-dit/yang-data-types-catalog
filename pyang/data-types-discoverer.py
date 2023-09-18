b1992e5dbb2bb32863a890488078e759e19a394a
David Martínez García <94689492+martinezgarciadavid@users.noreply.github.com>
2023-09-18 13:30:44 +0200

    Update pyang plugin and catalog files

--- pyang/data-types-discoverer.py
+++ pyang/data-types-discoverer.py
@@ -5,7 +5,7 @@ Given a YANG module, it searches for its typedefs, generates their translations 
 primite YANG types and outputs the result in JSON format.
 This plugin is meant to be used with the data types catalog available at https://github.com/giros-dit/yang-data-types-catalog.
 
-Version: 0.0.1.
+Version: 0.0.3.
 
 Author: Networking and Virtualization Research Group (GIROS DIT-UPM) -- https://dit.upm.es/~giros.
 '''
@@ -61,7 +61,7 @@ This plugin is meant to be used with the data types catalog available at https:/
 
 Usage:
 pyang -f data-types-discoverer <module.yang>
-          ''')
+    ''')
 
 def generate_output(ctx, modules, fd):
     '''
@@ -72,36 +72,29 @@ def generate_output(ctx, modules, fd):
 
     # CONSTANTS:
 
-    CATALOG_FILE_VERSION = "0.0.1"
-
-    PYANG_DATA_TYPES_DISCOVERER_VERSION = "0.0.1"
-
     YANG_PRIMITIVE_TYPES = [
         "int8", "int16", "int32", "int64",
         "uint8", "uint16", "uint32", "uint64",
         "decimal64", "string", "boolean", "enumeration",
         "bits", "binary", "empty", "union"
-        ]
+    ]
 
     for module in modules:
         data = {}
 
         module_name = str(module.i_modulename)
-        data["name"] = module_name
+        data["module_name"] = module_name
 
         module_prefix = str(module.i_prefix)
-        data["prefix"] = module_prefix
+        data["module_prefix"] = module_prefix
 
         module_namespace = str(module.search_one("namespace").arg)
-        data["namespace"] = module_namespace
+        data["module_namespace"] = module_namespace
 
         module_latest_revision = str(module.i_latest_revision)
-        data["revision"] = module_latest_revision
+        data["module_revision"] = module_latest_revision
 
-        data["catalog_file_version"] = CATALOG_FILE_VERSION
-        data["pyang_data_types_discoverer_version"] = PYANG_DATA_TYPES_DISCOVERER_VERSION
-
-        data["typedefs"] = {}
+        data["module_typedefs"] = {}
 
         typedefs = module.search("typedef")
 
@@ -110,22 +103,24 @@ def generate_output(ctx, modules, fd):
         if typedefs is not None:
             for typedef in typedefs:
                 if typedef is not None:
-                    typedefs_dict[str(typedef.arg)] = str(typedef.search_one("type").arg).replace(module_prefix + ":", "")
+                    typedef_name = str(typedef.arg)
+                    typedef_type = str(typedef.search_one("type").arg)
+                    typedefs_dict[typedef_name] = typedef_type
 
             for typedef in typedefs:
                 if typedef is not None:
                     typedef_name = str(typedef.arg)
-                    data["typedefs"][typedef_name] = {}
+                    data["module_typedefs"][typedef_name] = {}
                     
-                    typedef_type = str(typedef.search_one("type").arg).replace(module_prefix + ":", "")
-                    data["typedefs"][typedef_name]["type"] = typedef_type
+                    typedef_type = str(typedef.search_one("type").arg)
+                    data["module_typedefs"][typedef_name]["defined_type"] = typedef_type
 
                     typedef_primitive_type = ""
                     if typedef_type not in YANG_PRIMITIVE_TYPES:
-                        typedef_primitive_type = typedefs_dict[typedef_type]
+                        typedef_primitive_type = typedefs_dict[typedef_type.replace(module_prefix + ":", "")]
                     else:
                         typedef_primitive_type = typedef_type
-                    data["typedefs"][str(typedef.arg)]["primitive_type"] = typedef_primitive_type
+                    data["module_typedefs"][typedef_name]["primitive_type"] = typedef_primitive_type
         
         filename = module_name + "@" + module_latest_revision + ".json"
         file = open(filename, 'w')
