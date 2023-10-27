e91ff4633306e78fcdd4adeed730bb902565965d
David Martínez García <94689492+david-martinez-garcia@users.noreply.github.com>
2023-10-27 08:56:18 +0200

    Some updates and improvements (see description)
    - Pyang plugin updated with improvements when discovering typedefs. It now considers the description for every typedef and includes it in the output file.
    - Catalog files updated accordingly.

--- pyang/data-types-discoverer.py
+++ pyang/data-types-discoverer.py
@@ -5,7 +5,7 @@ Given a YANG module, it searches for its typedefs, generates their translations 
 primitive YANG types and outputs the result in JSON format.
 This plugin is meant to be used with the data types catalog available at https://github.com/giros-dit/yang-data-types-catalog.
 
-Version: 0.0.5.
+Version: 0.0.7.
 
 Author: Networking and Virtualization Research Group (GIROS DIT-UPM) -- https://dit.upm.es/~giros.
 '''
@@ -55,11 +55,11 @@ def print_data_types_discoverer_help():
 Pyang plugin - YANG data types discoverer (data-types-discoverer).
 Given a YANG module, it searches for its typedefs, generates their translations to
 primitive YANG types and outputs the result in JSON format.
-The output is written to stdout and, therefore, printed to the terminal. If you want the output to be written to a file, just redirect it accordingly.
+The output is directly written to a file with the following name: <module_name>@<module_revision>.json
 This plugin is meant to be used with the data types catalog available at https://github.com/giros-dit/yang-data-types-catalog.
 
 Usage:
-pyang -f data-types-discoverer <module.yang> [> <output_file.json>]
+pyang -f data-types-discoverer <module.yang>
     ''')
 
 def generate_output(ctx, modules, fd):
@@ -68,8 +68,9 @@ def generate_output(ctx, modules, fd):
     '''
 
     # Use PDB to debug the code with pdb.set_trace().
+    # pdb.set_trace()
 
-    # CONSTANTS:
+    ### CONSTANTS ###
 
     YANG_PRIMITIVE_TYPES = [
         "int8", "int16", "int32", "int64",
@@ -78,49 +79,62 @@ def generate_output(ctx, modules, fd):
         "bits", "binary", "empty", "union"
     ]
 
+    ### --- ###
+
+    ### AUXILIARY FUNCTIONS ###
+
+    def is_deprecated(typedef) -> bool:
+        '''
+        Auxiliary function.
+        Checks if a typedef is deprecated.
+        '''
+        result = False
+        status = typedef.search_one('status')
+        if (status is not None) and (status.arg == 'deprecated'):
+            result = True
+        return result
+
+    ### --- ###
+
+    # Process modules to discover typedefs. Output is in JSON format.
     for module in modules:
         data = {}
-
         module_name = str(module.i_modulename)
         data["module_name"] = module_name
-
         module_prefix = str(module.i_prefix)
         data["module_prefix"] = module_prefix
-
         module_namespace = str(module.search_one("namespace").arg)
         data["module_namespace"] = module_namespace
-
         module_latest_revision = str(module.i_latest_revision)
         data["module_revision"] = module_latest_revision
-
         data["module_typedefs"] = {}
 
         typedefs = module.search("typedef")
-
         typedefs_dict = {}
-
         if typedefs is not None:
-            for typedef in typedefs:
-                if typedef is not None:
+            for typedef in typedefs: # First iteration retrieves the defined type.
+                if (typedef is not None) and (is_deprecated(typedef) == False):
                     typedef_name = str(typedef.arg)
-                    typedef_type = str(typedef.search_one("type").arg)
+                    typedef_type = str(typedef.search_one("type").arg).split(':')[-1]
                     typedefs_dict[typedef_name] = typedef_type
-
-            for typedef in typedefs:
-                if typedef is not None:
+            for typedef in typedefs: # Second iteration retrieves the primitive type.
+                if (typedef is not None) and (is_deprecated(typedef) == False):
                     typedef_name = str(typedef.arg)
+                    typedef_description = str(typedef.search_one("description").arg)\
+                        .replace('\n', ' ').replace('  ', ' ')
+                    typedef_type = str(typedef.search_one("type").arg).split(':')[-1]
                     data["module_typedefs"][typedef_name] = {}
-                    
-                    typedef_type = str(typedef.search_one("type").arg)
+                    data["module_typedefs"][typedef_name]["description"] = typedef_description
                     data["module_typedefs"][typedef_name]["defined_type"] = typedef_type
-
-                    typedef_primitive_type = ""
                     if typedef_type not in YANG_PRIMITIVE_TYPES:
-                        typedef_primitive_type = typedefs_dict[typedef_type.replace(module_prefix + ":", "")]
+                        if typedefs_dict.get(typedef_type) is not None:
+                            typedef_primitive_type = typedefs_dict[typedef_type]
+                        else:
+                            typedef_primitive_type = typedef_type
                     else:
                         typedef_primitive_type = typedef_type
                     data["module_typedefs"][typedef_name]["primitive_type"] = typedef_primitive_type
         
-        fd.write(json.dumps(data, indent=4) + '\n')
-
-        fd.close()
+        output_file = open(module_name + "@" + module_latest_revision + ".json", "w")
+        output_file.write(json.dumps(data, indent=4) + '\n')
+        output_file.close()
